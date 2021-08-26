d126b47a87f6e632987729091ef484b88f116333
David Castillo <vikruzdavid@gmail.com>
2021-08-26 16:50:59 -0400

    q

--- index.js
+++ index.js
@@ -1,53 +1,58 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-//pinta un mensaje de error segun el argumento mensaje_error
-function mensaje(mensaje_error){
-	var html = `
-    <div class='notification is-danger'>
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    //pinta un mensaje de error segun el argumento mensaje_error
+    function mensaje(mensaje_error) {
+      var html = `
+    <div class='notification is-danger mb-1'>
     	<h4 class='title is-4'> ${mensaje_error}</h4> 
 	</div>
-	`
-	document.getElementById("error").innerHTML += html;
-}
+	`;
+      document.getElementById("error").innerHTML += html;
+    }
 
-//esta funcion valida el correo electronico
-function validarEmail(email) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
+    //esta funcion valida el correo electronico
+    function esValido(email) {
+      const re =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(email).toLowerCase());
+    }
 
-//valida todos las entradas
-function validar(){
- 	// variable de email
-	var email = document.getElementById("correo").value;
-	// variable de password
-	var password = document.getElementById("password").value;
+    //valida todos las entradas
+    function validar() {
+      // variable de email
+      const email = document.getElementById("correo").value;
+      // variable de password
+      const password = document.getElementById("password").value;
 
-	//Si email esta vacio.
-	if (email === ''){
-		mensaje("Escriba el email");
-	} 
-	//Si Password esta vacio.
-	if (password === ''){
-		mensaje("Escriba el password");
-	} 
+      //Si email esta vacio.
+      if (email === "") {
+        mensaje("Escriba el email");
+      }
+      //Si Password esta vacio.
+      if (password === "") {
+        mensaje("Escriba el password");
+      }
 
-	//Si el Email esta mal escrito.
-	if (validarEmail(email)==false){
-		mensaje("Mail esta mal escrito")
-	}
+      //Si el Email esta mal escrito.
+      if (!esValido(email)) {
+        mensaje("Mail esta mal escrito");
+      }
 
-	//validar que el corro y el password sean correctos.
-	if ( email == "patricia@gmail.com" && password == "11417592"){
-		window.location.href = "./exito.html"
-	}else{
-		mensaje("Correo electronico o password invalido.");
-	}
-}
+      //validar que el corro y el password sean correctos.
+      if (email == "patricia@gmail.com" && password == "11417592") {
+        window.location.href = "./exito.html";
+      } else {
+        mensaje("Correo electronico o password invalido.");
+      }
+    }
 
-	//Evento on click para el boton de iniciar sesión
-	document.getElementById("btnEnviar").addEventListener('click', function() {
-		document.getElementById("error").innerHTML = "";
-		validar();
-	})
-}, false);
+    //Evento on click para el boton de iniciar sesión
+    document.getElementById("login_form").addEventListener("submit", (e) => {
+      e.preventDefault();
+      document.getElementById("error").innerHTML = "";
+      validar();
+    });
+  },
+  false
+);
